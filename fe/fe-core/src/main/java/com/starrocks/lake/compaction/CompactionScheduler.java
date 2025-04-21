8c2f8da1bc94c262e94a10a587011aa4101f2359
zhangqiang <qiangzh95@gmail.com>
2025-04-21 10:54:34 +0800

    [Stella][BP-Enhancement] Add storage size property for cloud native partition (#56234)
    Signed-off-by: sevev <qiangzh95@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/lake/compaction/CompactionScheduler.java
+++ fe/fe-core/src/main/java/com/starrocks/lake/compaction/CompactionScheduler.java
@@ -515,6 +515,7 @@ public class CompactionScheduler extends Daemon {
             }
             waiter = transactionMgr.commitTransaction(db.getId(), job.getTxnId(), commitInfoList,
                     Collections.emptyList(), attachment);
+            job.getPartition().incExtraFileSize(job.getSuccessCompactInputFileSize());
         } finally {
             locker.unLockTablesWithIntensiveDbLock(db, tableIdList, LockType.WRITE);
         }
