3221ccdaae854af707085a23bc4ecae25d49ad1e
zqy455486 <zqy455486@alibaba-inc.com>
2025-12-02 15:02:45 +0800

    [Stella][Enhancement] Support skip json parse error for routine load Link: https://code.alibaba-inc.com/soe/stella/codereview/24678000 * [Stella][Enhancement] Support skip json parse error for routine load
    (cherry picked from commit f540e5b9)

--- be/src/exec/json_scanner.h
+++ be/src/exec/json_scanner.h
@@ -127,6 +127,10 @@ private:
 
     void _append_error_msg(const std::string&, const std::string& error_msg);
 
+    bool _is_routine_load();
+
+    std::string _get_current_meta_msg();
+
 private:
     RuntimeState* _state = nullptr;
     ScannerCounter* _counter = nullptr;
